246cd67acded9e2f638a6e6d3207ad44f1746374
voephong <voephoung27@gmail.com>
2024-06-03 06:40:31 +0000

    hh

---
+++ index.php
@@ -0,0 +1,5 @@
+
+
+a href="login.php">Login</a>
+<a href="register.php">Register</a>
+
---
+++ register.php
@@ -0,0 +1,37 @@
+<?php
+include 'db.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Registration successful!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Register</title>
+</head>
+<body>
+    <h2>Register</h2>
+    <form method="post" action="">
+        Username: <input type="text" name="username" required><br>
+        Password: <input type="password" name="password" required><br>
+        <button type="submit">Register</button>
+       <button>
+        <a href="login.php">Cancel</a>
+</button>
+
+ </form>
+</body>
+</html>
